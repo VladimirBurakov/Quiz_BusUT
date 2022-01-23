9d3482b5a2a2fe7cf174732f4ceda4d633123893
VladimirBurakov <burakov.vn81@gmail.com>
2022-01-24 00:23:45 +0200

    reserve point, first working version TestDbHandler

---
+++ src/sample/globalconstants/FileConst.java
@@ -0,0 +1,6 @@
+package sample.controllers;
+
+public class FileConst {
+    public final static String JSON_FILE = "D:\\java_projects\\Quiz\\src\\sample\\dao\\JSON_BAS_UT_Base.json";
+    public final static String CSV_FILE = "D:\\java_projects\\Quiz\\src\\sample\\dao\\BAS_UT_Base.csv";
+}
